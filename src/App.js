6fa8b6c723c8a55d3ffbba443d3351000acdc29c
Matt <mddever@gmail.com>
2019-07-12 18:38:10 -0600

    replaced App.js with State and Hook for state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  /// Hook goes above return, below App function
+
+  const [userText, setUserText] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+      <h2>Type Race</h2>
+      <input />
+      </div>
+  )
 }
-
 export default App;
